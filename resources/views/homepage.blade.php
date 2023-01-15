a7942e2c607cf37bfa3920d6ff100c634b71f8f4
Vale-riad <valentinariad91@gmail.com>
2023-01-15 11:08:20 +0100

    Aggiunge comics

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,3 +2,28 @@
 @section('page-title')
    Homepage 
 @endsection
+@section("page-content")
+<div class="my-bg">
+    <section class="container">
+        <div class="comics">
+            <button class="btn-series">
+                <h2>CURRENT SERIES</h2>
+            </button>
+
+            @foreach ($comics as $comic)
+            <div class="comic" >
+                <img src="{{$comic['thumb']}}" alt="">
+                <h5>{{$comic['series']}}</h5>
+            </div>
+            @endforeach
+                
+        </div>
+        <div class="d-flex">
+            <button class="btn-load">
+                LOAD MORE
+            </button>
+        </div>
+    </section>
+</div>
+@endsection
+
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,19 +1,24 @@
 <header>
     <div class="container d-flex justify-content-between align-items-center mb-2">
         <img src="{{Vite::asset("resources/img/dc-logo.png")}}">
+        <nav class="ms-nav">
             <ul class="d-flex list-unstyled">
-                <li><a class="menu-item" href=""></a>Characters</li>
-                <li><a class="menu-item" href=""></a>Characters</li>
-                <li><a class="menu-item"href=""></a>Characters</li>
-                <li><a class="menu-item" href=""></a>Characters</li>
-                <li><a class="menu-item" href=""></a>Characters</li>
-                <li><a class="menu-item" href=""></a>Characters</li>
-                <li><a class="menu-item" href=""></a>Characters</li>
+                <li><a class="menu-item" href="#"></a>CHARACTERS</li>
+                <li><a class="menu-item" href="#"></a>COMICS</li>
+                <li><a class="menu-item" href="#"></a>MOVIES</li>
+                <li><a class="menu-item" href="#"></a>TV</li>
+                <li><a class="menu-item" href="#"></a>GAMES</li>
+                <li><a class="menu-item" href="#"></a>COLLECTIBLESS</li>
+                <li><a class="menu-item" href="#"></a>VIDEOS</li>
+                <li><a class="menu-item" href="#"></a>FANS</li>
+                <li><a class="menu-item" href="#"></a>NEWS</li>
+                <li><a class="menu-item" href="#"></a>SHOP</li>
             </ul>
-            <div>
-                </small>Search</small>
-                <i class="fa-regular fa-magnifying-glass"></i>
-            </div>
+        </nav>
+        <div>
+        <span>Search</span>
+            <i class="fa-regular fa-magnifying-glass"></i>
+        </div>
     </div>  
     <div class="hero">
     </div>  
